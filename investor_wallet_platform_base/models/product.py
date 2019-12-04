a7b0fb11557c37d366721e3a7e236281897c7aef
Rémy Taymans <remytaymans@gmail.com>
2019-12-04 21:18:24 +0100

    [ADD] iwp_base: Min and max for share subscription

--- investor_wallet_platform_base/models/product.py
+++ investor_wallet_platform_base/models/product.py
@@ -79,3 +79,51 @@ class ProductTemplate(models.Model):
             ("state", "=", "open"),
             ("display_on_website", "=", True),
         ])
+
+    @api.multi
+    def can_buy_max_amount(self, partner_id):
+        """
+        Return the amount that a partner can subscribe on for this type
+        of share product.
+        Return a negative number if there is not limit.
+        """
+        self.ensure_one()
+        company = self.env["res.company"]._company_default_get()
+        amount_owned_structure = sum(
+            sl.total_amount_line
+            for sl in partner_id.share_ids
+            if sl.structure == self.structure
+        )
+        amount_owned_share = self.owned_amount(partner_id)
+        max_subscription = (
+            self.structure.subscription_maximum_amount
+            or company.subscription_maximum_amount
+        )
+        # First amount left regarding to the general max subscription
+        if max_subscription > 0:
+            left_amount = max_subscription - amount_owned_structure
+            # Then amount left for this type of share
+            if left_amount > 0:
+                return max(0, self.maximum_amount - amount_owned_share)
+            return 0
+        return -1  # No limit
+
+    @api.multi
+    def can_buy_min_amount(self, partner_id):
+        """
+        Return the minimum amount that a partner have to subscribe.
+        """
+        self.ensure_one()
+        return max(0, self.minimum_amount - self.owned_amount(partner_id))
+
+    @api.multi
+    def owned_amount(self, partner_id):
+        """
+        Return the amount of this type of share owned by the given
+        partner.
+        """
+        return sum(
+            sl.total_amount_line
+            for sl in partner_id.share_ids
+            if sl.share_product_id == self.product_variant_id
+        )
